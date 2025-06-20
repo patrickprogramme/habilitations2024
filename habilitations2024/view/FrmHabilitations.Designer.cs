26c96f519ab22914ca3dce3591870007eb505446
Patrick <patrick@patrick.com>
2025-06-20 19:45:21 +0200

    Ajoutez des fichiers projet.

---
+++ habilitations2024/view/FrmHabilitations.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace habilitations2024.view
+{
+    partial class FrmHabilitations
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmHabilitations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmHabilitations";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmHabilitations_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ habilitations2024/view/FrmHabilitations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace habilitations2024.view
+{
+    public partial class FrmHabilitations : Form
+    {
+        public FrmHabilitations()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmHabilitations_Load(object sender, EventArgs e)
+        {
+            string stringConnect = "server=localhost;user=habilitations;database=habilitations;port=3306;password=motdepasseuser;";
+        }
+    }
+}
